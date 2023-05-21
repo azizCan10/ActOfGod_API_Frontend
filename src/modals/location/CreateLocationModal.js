3ae8d84ce3245ff8920e7acfed1898c1dc2b17e9
coz <can.oz@32bit.com.tr>
2023-05-21 18:09:58 +0300

    lat and lng infos taken from google map

--- src/modals/location/CreateLocationModal.js
+++ src/modals/location/CreateLocationModal.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 
 import axios from "axios";
 
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
 
 /**
  * This function defines adding location modal
  */
-export default function CreateLocationModal({ createLocation, setCreateLocation }) {
+export default function CreateLocationModal({createLocation, setCreateLocation}) {
 
     //sets location parameters according to inputs
     const onLocationChange = (e) => {
-        setCreateLocation({ ...createLocation, [e.target.name]: e.target.value });
+        setCreateLocation({...createLocation, [e.target.name]: e.target.value});
     }
 
     //district list
@@ -37,6 +37,16 @@ export default function CreateLocationModal({ createLocation, setCreateLocation 
         lng: -38.523
     };
 
+    const handleMapClick = (event) => {
+        const lat = event.latLng.lat();
+        const lng = event.latLng.lng();
+        setXY(lat, lng);
+    };
+
+    const setXY = (x, y) => {
+        setCreateLocation({...createLocation, latitude: x, longitude: y});
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -44,26 +54,30 @@ export default function CreateLocationModal({ createLocation, setCreateLocation 
                     <form>
                         <div className="mb-3">
                             <label htmlFor="name" className="form-label">Adres Adı</label>
-                            <input type={"text"} className="form-control" name="name" value={createLocation.name} onChange={(e) => onLocationChange(e)} />
+                            <input type={"text"} className="form-control" name="name" value={createLocation.name}
+                                   onChange={(e) => onLocationChange(e)}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="capacity" className="form-label">Kapasite</label>
-                            <input type={"text"} className="form-control" name="capacity" value={createLocation.capacity} onChange={(e) => onLocationChange(e)} />
+                            <input type={"text"} className="form-control" name="capacity"
+                                   value={createLocation.capacity} onChange={(e) => onLocationChange(e)}/>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="districtId" className="form-label">İlçe Adı</label>
-                            <select className="form-select" id="exampleFormControlSelect1"  name="districtId" onChange={(e) => onLocationChange(e)}>
+                            <select className="form-select" id="exampleFormControlSelect1" name="districtId"
+                                    onChange={(e) => onLocationChange(e)}>
                                 {districtList.map((district) => (
                                     <option value={district.id}>{district.name}</option>
                                 ))}
                             </select>
                         </div>
                         <div className="mb-3">
-                            <LoadScript googleMapsApiKey="AIzaSyABjQxjbQdIqytvaX7PtkQXX8rbbMpglUQ">
+                            <LoadScript googleMapsApiKey="AIzaSyAd4VpzsRvbAEE2vXpKYUud_J0tDxW8zmE">
                                 <GoogleMap
                                     mapContainerStyle={containerStyle}
                                     center={center}
                                     zoom={10}
+                                    onClick={handleMapClick}
                                 >
                                 </GoogleMap>
                             </LoadScript>
